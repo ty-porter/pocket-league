5805c3bec219c7a5cc3bf2fed3907894ba46d2b2
pawptart <tyler.b.porter@gmail.com>
2021-03-28 19:39:43 -0400

    Add ball physics

--- gb_pocket_league.c
+++ gb_pocket_league.c
@@ -4,27 +4,36 @@
 
 #include <stdio.h>
 #include <rand.h>
+#include <stdlib.h>
 
 #include "sprites/car.c"
 #include "sprites/boost.c"
+#include "sprites/ball.c"
+
 #include "backgrounds/arena.h"
 
 #define FLOOR               140u
 #define CEILING             24u
 #define GAME_SPEED          3 // Higher is slower
+#define BALL_FLOOR          136u    
 
 #define ARENA_X_MIN         16
 #define ARENA_X_MAX         144
 
 #define GRAVITY             1
-#define MAX_VELOCITY        5
 #define JUMP_ACCELERATION   10
 #define ACCELERATION        1
 #define ROTATION_SPEED      15
 #define BOOST_ACCELERATION  1
 
-#define CAR_1_START_X       64
-#define CAR_1_START_y       64
+#define NUM_CARS            1
+#define CAR_1_START_X       20
+#define CAR_1_START_y       136
+
+#define BALL_START_X        80
+#define BALL_VELOCITY       2
+#define BALL_BUMP_VERT      10 // Slight upward velocity for hitting the ball
+#define BOUNCE_DECAY        5
 
 /* Rolls the car clockwise based on *rot* param (INT8 0 - 255) */
 void draw_car_roll(INT8 n, UINT8 rot) {
@@ -172,7 +181,7 @@ void kill_boost_sprite(UINT8 n) {
     move_sprite((n * 4) + 4, 255, 255); // offscreen
 }
 
-void move_car_sprite(INT8 n, INT8 x, INT8 y, INT8 rot) {
+void move_car_sprite(UINT8 n, UINT8 x, UINT8 y, UINT8 rot) {
     INT8 spr_offset = n * 4;
 
     move_sprite(0 + spr_offset, x,     y    );
@@ -196,12 +205,42 @@ void initialize_cars(INT8 n) {
             }
         }
 
-        move_car_sprite(i, 32 * 2 * (i + 1), 64, 0);
+        if (i == 1) {
+            move_car_sprite(i, CAR_1_START_X, CAR_1_START_y, 0);
+        } 
+        else {
+            // TODO: Refactor for 2 cars
+        }
     }
 }
 
 INT8 debounced_input(INT8 target, INT8 new, INT8 old) {
     return (new & target) && !(old & target);
+}
+
+void move_ball_sprite(UINT8 x, UINT8 y, INT8 t) {
+    UINT8 sprite_start = NUM_CARS * 4 + 1;
+
+    for (INT8 i = 0; i < 4; i++) {
+        set_sprite_tile(sprite_start + i, t % 2 == 0 ? 30 : 31);
+    }
+
+    move_sprite(sprite_start, x, y);
+
+    move_sprite(sprite_start + 1, x, y + 8);
+    set_sprite_prop(sprite_start + 1, S_FLIPY);
+
+    move_sprite(sprite_start + 2, x + 8, y);
+    set_sprite_prop(sprite_start + 2, S_FLIPX);
+    
+    move_sprite(sprite_start + 3, x + 8, y + 8);
+    set_sprite_prop(sprite_start + 3, S_FLIPY | S_FLIPX);
+}
+
+void initialize_ball() {
+    set_sprite_data(30, 2, ball);
+    
+    move_ball_sprite(BALL_START_X, BALL_FLOOR, 0);
 }
 
 void initialize_background() {
@@ -259,13 +298,21 @@ void calculate_boost_velocity_vectors(UINT8 rot, INT8 *d_x, INT8 *d_y) {
     *d_y += BOOST_ACCELERATION * y_mod;
 }
 
+void calculate_ball_velocity_vectors(UINT8 x, UINT8 y, INT8 d_x, INT8 d_y, UINT8 ball_x, UINT8 ball_y, INT8 *ball_d_x, INT8 *ball_d_y) {
+    if ((x <= ball_x + 16 && x >= ball_x - 16) && (y <= ball_y + 11 && y >= ball_y - 5)) {
+        *ball_d_x = d_x * BALL_VELOCITY;
+        *ball_d_y = d_y - (BALL_BUMP_VERT * abs(d_x) / 10);
+    }
+}
+
 void main() {
     BGP_REG = OBP0_REG = OBP1_REG = 0xE4;
 
     SPRITES_8x8;
 
     initialize_background();
-    initialize_cars(1);
+    initialize_cars(NUM_CARS);
+    initialize_ball();
 
     SHOW_BKG; SHOW_SPRITES;
 
@@ -275,8 +322,14 @@ void main() {
     INT8 d_x = 0;
     INT8 d_y = 0;
 
+    INT8 ball_d_x = 0;
+    INT8 ball_d_y = 0;
+
     UINT8 x_pos = CAR_1_START_X;
     UINT8 y_pos = CAR_1_START_y;
+
+    UINT8 ball_x_pos = BALL_START_X;
+    UINT8 ball_y_pos = BALL_FLOOR;
 
     UINT8 rotation = 0;
     UINT8 tick = 0;
@@ -364,7 +417,70 @@ void main() {
             d_y = 0;
         }
 
+
+        if (ball_d_x > 0) {
+            if (ball_d_x - ACCELERATION >= 0) {
+                ball_d_x -= ACCELERATION;
+            }
+            else {
+                ball_d_x = 0;
+            }
+        } 
+        else if (ball_d_x < 0) {
+            if (ball_d_x + ACCELERATION <= 0) {
+                ball_d_x += ACCELERATION;
+            }
+            else {
+                ball_d_x = 0;
+            }
+        }
+
+        ball_d_y += GRAVITY;
+
+        if (ball_y_pos + ball_d_y > BALL_FLOOR) {
+            ball_y_pos = BALL_FLOOR;
+            ball_d_y = (ball_d_y * -1) + (BOUNCE_DECAY * GRAVITY);
+
+            if (ball_d_y > 0) {
+                ball_d_y = 0;
+            }
+        }
+        else if (ball_y_pos + ball_d_y < CEILING) {
+            ball_d_y *= -1;
+            ball_y_pos = CEILING;
+        }
+
+        calculate_ball_velocity_vectors(x_pos, y_pos, d_x, d_y, ball_x_pos, ball_y_pos, &ball_d_x, &ball_d_y);
+
+        // Handle overflow errors
+        if (abs(ball_d_x) >= ball_x_pos && ball_d_x < 0) {
+            ball_x_pos = ARENA_X_MIN;
+            ball_d_x *= -1;
+        }
+        else if (ball_d_x + ball_x_pos > 255) {
+            ball_x_pos = ARENA_X_MAX;
+            ball_d_x *= -1;
+        }
+
+        if (ball_y_pos < abs(ball_d_y) && ball_d_y < 0) {
+            ball_y_pos = CEILING;
+            ball_d_y *= -1;
+        }
+
+        ball_x_pos += ball_d_x;
+        ball_y_pos += ball_d_y;
+
+        if (ball_x_pos > ARENA_X_MAX) {
+            ball_x_pos = ARENA_X_MAX;
+            ball_d_x *= -1;
+        }
+        else if (ball_x_pos < ARENA_X_MIN) {
+            ball_x_pos = ARENA_X_MIN;
+            ball_d_x *= -1;
+        }
+
         move_car_sprite(0, x_pos, y_pos, rotation);
+        move_ball_sprite(ball_x_pos, ball_y_pos, ball_d_x | ball_d_y ? tick : 0); // Move sprite to position, check if moving for sprite updates
 
         if (key1 & J_B) {
             draw_boost_sprite(0, x_pos, y_pos, rotation, tick % 2);
